0bdbfb95945d9707e73e048e3c048b67c8fb44bb
naouflex <naoufel.eth@gmail.com>
2024-11-13 17:04:46 +0000

    branding 3

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -99,7 +99,7 @@ for source in log_sources:
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "Inverse Chat")
 #if WEBUI_NAME != "Open WebUI":
 #    WEBUI_NAME += " (Open WebUI)"
 
